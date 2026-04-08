78964ba17361ad7331e7e6920e9eb562d3c06ff3
Rosa2401 <Bosa2401@gmail.com>
2026-04-08 11:22:43 +0200

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import Lottie from "lottie-react";
+import animationData from "../assets/hero-forside-6.json";
+
+export default function HeroSection() {
+  return (
+    <section className="relative w-full h-screen flex items-center justify-center">
+      {/* Lottie Background */}
+      <div className="absolute inset-0">
+        <Lottie
+          animationData={animationData}
+          loop={false}
+          autoplay={true}
+          rendererSettings={{ preserveAspectRatio: "xMidYMid meet", progressiveLoad: true }}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+    </section>
+  );
+}
