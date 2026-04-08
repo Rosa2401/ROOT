032f7773ef6f70190ae985753216dad0b637c925
Rosa2401 <Bosa2401@gmail.com>
2026-04-08 13:18:42 +0200

    Update HeroTwo.jsx

--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -13,8 +13,10 @@ export default function HeroSectionSVG() {
 
         {/* Centered Text */}
         <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
-          <h1 className="text-[#442F2A] font-oleo text-4xl font-bold text-center">Cases</h1>
-          <p className="text-[#442F2A] text-lg text-center max-w-2xl">
+          <h1 className="text-[#442F2A] font-oleo text-9xl font-bold text-center">
+  Cases
+</h1>
+          <p className="text-[#442F2A] text-lg text-center max-w-2xl font-semibold w-120">
             Vi har hjulpet flere vrksomheder med at skabe deres visuelle identitet, som afspejler hvem de er.
           </p>
         </div>
