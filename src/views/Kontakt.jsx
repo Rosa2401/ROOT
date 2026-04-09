8b78b25db3c7b1ac8928167566a78354d8160f4a
Rosa2401 <Bosa2401@gmail.com>
2026-04-09 09:58:07 +0200

    Update Kontakt.jsx

--- src/views/Kontakt.jsx
+++ src/views/Kontakt.jsx
@@ -8,9 +8,9 @@ export default function Kontakt() {
       <div>
         <HeroSectionSVG
                         title="Skal vi hjælpe dig?"
-                        description="Vi står klar til at hjælpe dig med at skabe en løsning, 
-                        der både fungerer og ser godt ud. Fortæl os om dit projekt, 
-                        så finder vi den bedste vej frem sammen."
+                        description="Vi står klar til at hjælpe dig med at skabe en 
+                        løsning, der både fungerer og ser godt ud. Fortæl os om dit 
+                        projekt, så finder vi den bedste vej frem sammen."
         />    
         <FiftyR {...FiftyRIndhold.uforpligtende} />
         <Formular />
